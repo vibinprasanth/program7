1268223b87f2efc850fbf2734feedcd4dc307a88
vibinprasanth <infovibinprasanth@gmail.com>
2026-09-23 12:31:21 +0530

    Implement firewall zone commands in script

--- firewall_zones.sh
+++ firewall_zones.sh
@@ -4,21 +4,21 @@
 # Register Number:
 
 # 1. Display all available firewall zones
-# TODO
+firewall-cmd --get-zones
 
 # 2. Display current default zone
-# TODO
+firewall-cmd --get-default-zone
 
 
 # 3. Display information about public zone
-# TODO
+firewall-cmd --zone=public --list-all
 
 # 4. Set default zone to internal
-# TODO
+firewall-cmd --set-default-zone=internal
 
 
 # 5. Verify the new default zone
-# TODO
+firewall-cmd --get-default-zone
 
 
 
